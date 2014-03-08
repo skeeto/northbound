15c5a7793b75829770fb249dd7b9a28bcabfbe2e
Christopher Wellons <wellons@nullprogram.com>
2014-03-08 16:22:25 -0500

    Add some music.

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 
 An entry for [7DRL 2014][7drl].
 
+## Graphics
+
+* [Quaade](http://opengameart.org/content/sprites-for-top-down-rpg) (CC-BY 3.0)
+
+## Music
+
+* [Kevin MacLeod](http://incompetech.com/music/royalty-free/) (CC-BY 3.0)
+
 
 [7drl]: http://www.roguebasin.com/index.php?title=7DRL_Challenge_2014
--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
         display.draw(ctx);
     });
     game.message('Escape northward!');
+    music();
 
     function step() {
         if (!game.isDone()) {
@@ -20,3 +21,35 @@ $(document).ready(function() {
     }
     game.step(step);
 });
+
+Array.prototype.shuffle = function(){
+    for(var j, x, i = this.length; i;
+        j = Math.floor(Math.random() * i),
+        x = this[--i], this[i] = this[j], this[j] = x);
+    return this;
+};
+
+var MUSIC = [
+    ["Dark Times.ogg", "Dark Times.mp3"],
+    ["Death of Kings.ogg", "Death of Kings.mp3"],
+    ["Enchanted Valley.ogg", "Enchanted Valley.mp3"],
+    ["End of the Era.ogg", "End of the Era.mp3"],
+    ["Lost Frontier.ogg", "Lost Frontier.mp3"],
+    ["The Pyre.ogg", "The Pyre.mp3"]
+].map(function(song) {
+    return song.map(function(file) {
+        return 'music/' + file;
+    });
+});
+
+var howl = null;
+function music() {
+    var music = MUSIC.shuffle()[0];
+    howl = new Howl({
+        urls: MUSIC.shuffle()[0],
+        autoplay: true,
+        loop: false,
+        volume: 0.25,
+        onend: music
+    });
+}
