d72214d9ab8dac8faa9dd0f987627a42088a78e3
Christopher Wellons <wellons@nullprogram.com>
2014-03-08 22:37:01 -0500

    Add story display.

--- src/Story.js
+++ src/Story.js
@@ -22,3 +22,40 @@ Story.scripts = {
         game.player.karma += n;
     }
 };
+
+Story.show = function(story) {
+    var title = story.title,
+        description = story.description.replace(/\n/g, '</p><p>');
+    $('#story .title').html(title);
+    $('#story .description').html('<p>' + description + '</p>');
+    var $options = $('#options');
+    $options.empty();
+    story.options.forEach(function(option) {
+        var $option = $('<li/>').addClass('option');
+        $option.html(option.answer);
+        $options.append($option);
+        $option.on('click', function() {
+            Story.act(option);
+        });
+    });
+    $('#story .close').hide();
+    $('#story').show();
+};
+
+Story.act = function(option) {
+    $('#story .description').html(option.result);
+    if (option.scripts) {
+        option.scripts.forEach(function(script) {
+            if (typeof script === "string") {
+                Story.scripts[script]();
+            } else {
+                Story.scripts[script[0]].apply(null, script.slice(1));
+            }
+        });
+    }
+    $('#options').empty();
+    $('#story .close').show().on('click', function() {
+        $('#story .close').hide().off('click');
+        $('#story').hide();
+    });
+};
