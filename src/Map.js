18e885ebee7a9fd405cb6709192428e0dc3d0365
Christopher Wellons <wellons@nullprogram.com>
2014-03-09 21:41:13 -0400

    Vary the terrain, too.

--- src/Map.js
+++ src/Map.js
@@ -13,15 +13,29 @@ Map.prototype.get = function(n) {
     return this.rows[n];
 };
 
-Map.SCALE = 8;
+Map.select = function(value) {
+    if (value > -0.4) {
+        return new Tile(Tile.GRASS);
+    } else if (value > -0.6) {
+        return new Tile(Tile.SOIL);
+    } else if (value > -0.9) {
+        return new Tile(Tile.DIRT);
+    } else {
+        return new Tile(Tile.SAND);
+    }
+};
+
+Map.SCALE = 25;
+Map.TREE_SCALE = 8;
 
 Map.prototype.generate = function(n) {
     while (this.rows.length < n + 1) {
         var row = [], y = this.rows.length;
         for (var x = 0; x < this.width; x++) {
-            var tile = new Tile(Tile.GRASS);
-            var limit = y < 10 ? (10 - y) : 0.25;
-            if (noise.perlin2(x / Map.SCALE, y / Map.SCALE) > limit) {
+            var value = noise.simplex2(x / Map.SCALE, y / Map.SCALE),
+                tile = Map.select(value),
+                limit = y < 10 ? (10 - y) : 0.22;
+            if (noise.perlin2(x / Map.TREE_SCALE, y / Map.TREE_SCALE) > limit) {
                 tile.obstacle = Tile.TREE;
             }
             row.push(tile);
